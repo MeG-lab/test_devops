a6a4fb1bd414939e47331eb90b0c6e3da9a3173c
Meteor <meteor@MeteordeMacBook-Air.local>
2026-05-22 12:40:21 +0800

    Revert "Reapply "Reapply "bad: 加了错误代码"""
    This reverts commit 7866a3cced6ec9aeffa84867fe1bea3661b7deba.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('node:http')
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+  res.end('<h1>Hello DIG!</h1>')
   res.end(`<h1>Hello DIG!</h1><p>时间：${new Date().toLocaleString()}</p>`)
 })
 
@@ -12,5 +13,3 @@ if (require.main === module) {
 }
 
 module.exports = server
-
-// 这是错误的代码！！！！
