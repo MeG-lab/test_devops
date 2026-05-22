0b716bd71993a0e167572a5bd7ed022f29939633
Meteor <meteor@MeteordeMacBook-Air.local>
2026-05-22 12:23:34 +0800

    页面增加服务器时间显示

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require('node:http')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+  res.end('<h1>Hello DIG!</h1>')
+  res.end(`<h1>Hello DIG!</h1><p>时间：${new Date().toLocaleString()}</p>`)
+})
+
+if (require.main === module) {
+  server.listen(3000, () => {
+    console.log('http://localhost:3000')
+  })
+}
+
+module.exports = server
